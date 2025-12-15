0352b252ca828f0bddd4fccd7eaa2e1d95241db6
Alex-Vicente11 <teiker_01@hotmail.com>
2025-12-15 01:09:22 -0600

    feat: Crear un singleton de RetrofitClient
    - Configurar Retrofit con la URL base
    - Añadir un interceptor de autenticación para el token de portador
    - Añadir un interceptor de registro para la depuración
    - Crear una instancia de apiService
    - Usar Gson para el análisis de JSON

---
+++ app/src/main/java/com/example/apptest/data/api/RetrofitClient.kt
@@ -0,0 +1,40 @@
+package com.example.apptest.data.api
+
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+
+    private const val BASE_URL = "https://api.themoviedb.org/3/"
+
+    private const val ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZWE3MjJiMjQ4YjU1N2E0Mzc1YmQ4OTUzYmU5ZjRmNyIsIm5iZiI6MTc2NTQ4MTM0Mi44NTMsInN1YiI6IjY5M2IxYjdlYTYzOGMzMzk3MjM2MDdlNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.k7Zpf2F7axUGqVU1rElMqK5iLlM6i5W2Qxa7Up6_iLY"
+
+    private val authInterceptor = Interceptor { chain ->
+        val newRequest = chain.request().newBuilder()
+            .addHeader("Authorization", "Bearer $ACCESS_TOKEN")
+            .addHeader("accept", "application/json")
+            .build()
+
+        chain.proceed(newRequest)
+    }
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(authInterceptor)
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val apiService: TMDDApiService = retrofit.create(TMDDApiService::class.java)
+}
