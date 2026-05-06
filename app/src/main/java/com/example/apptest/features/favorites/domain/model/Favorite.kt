6a26480b29635bc007b098556dbf9b6032739a95
Alex-Vicente11 <teiker_01@hotmail.com>
2026-05-06 01:00:17 -0600

    Crear feature favorites para sincronizar datos con Room

--- app/src/main/java/com/example/apptest/features/favorites/domain/model/Favorite.kt
+++ app/src/main/java/com/example/apptest/features/favorites/domain/model/Favorite.kt
@@ -1,4 +1,10 @@
 package com.example.apptest.features.favorites.domain.model
 
-class Favorite {
-}
+data class Favorite(
+    val id: Int,
+    val title: String,
+    val posterPath: String?,
+    val voteAverage: Double,
+    val releaseDate: String,
+    val overview: String,
+)
