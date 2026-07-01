2bbb27c9a383a0db37d1ccec8c827e94fa309a73
Alex-Vicente11 <teiker_01@hotmail.com>
2026-07-01 13:32:15 -0600

    Agregar archivo faltante AppErrorExt

--- app/src/main/java/com/alexvicente/moviedb/core/data/util/AppErrorExt.kt
+++ app/src/main/java/com/alexvicente/moviedb/core/data/util/AppErrorExt.kt
@@ -1,4 +1,11 @@
 package com.alexvicente.moviedb.core.data.util
 
-class AppErrorExt {
+fun AppError.toUserMessage(): String = when (this) {
+    is AppError.Network      -> message
+    is AppError.Unauthorized -> message
+    is AppError.NotFound     -> message
+    is AppError.RateLimited  -> message
+    is AppError.Server       -> message
+    is AppError.Database     -> message
+    is AppError.Unknown      -> message
 }
